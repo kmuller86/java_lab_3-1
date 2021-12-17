fdd767363c014a644a566bd4183743d3c8517e99
Szymon <szymondressler@gmail.com>
2021-12-17 12:13:27 +0100

    Cwiczenie_2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,49 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+
+        Student[] students = new Student [3];
+
+        Student b1 = new Student();
+
+        Student b2 = new Student();
+
+        Student b3 = new Student();
+
+        b1.Imie = "Szymon";
+
+        b1.Nazwisko = "Dressler";
+
+        b1.age = 29;
+        b1.young = true;
+
+        b1.displayInfo();
+
+        b2.Imie = "Janusz";
+
+        b2.Nazwisko = "Palikot";
+
+        b2.age = 21;
+
+        b2.young = true;
+
+        b2.displayInfo();
+
+        b3.Imie = "Mateusz";
+
+        b3.Nazwisko = "Byku";
+
+        b3.age = 34;
+
+        b3.young = true;
+
+        b3.displayInfo();
+
+
+
+        for (int i = 0;i >= students.length;++i){
+
+            System.out.println(students.length);
+        }
     }
 }
