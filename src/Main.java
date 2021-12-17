6c2e3e86ab0cd980767b6a287245ca04b15d410b
Szymon <szymondressler@gmail.com>
2021-12-17 12:23:45 +0100

    Cwiczenie_5

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,17 @@ public class Main {
             e) w klasie Main proszę stworzyć objekty podobnie jak w przykładzie 5,
             oraz stworzyć listę typu najbardziej generycznego np. List<Budynek>
          */
-
+        Polskie_Kluby team = new Polskie_Kluby("Arka Gdynia", "1929", 123.1);
+        System.out.println(team.toString());
+        Polskie team2 = new Team("Lech Poznan", "1920", 116.6);
+        System.out.println(team2.toString());
+        List<Polskie_Kluby> footballteam = new ArrayList<>();
+        footballteam.add(team);
+        footballteam.add(team2);
+        System.out.println(footballteam);
+        Object team3 = new Polskie_Kluby("Cracovia", "1906", 111,4);
+        System.out.println("\n" + team3.toString());
+        Object team4 = new Polskie_Kluby("Radomiak Radom", "1910", 101.6);
+        System.out.println(team4.toString());
     }
 }
